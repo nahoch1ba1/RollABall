07000c89e07f6a8dbab470b3723824d7323e4d85
千葉南朋 <vga29-ac220048@sankogakuen.jp>
2023-12-07 11:43:08 +0900

    コルーチン

---
+++ Assets/Scripts/RollABall/InGame/Coin.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private float bigBonusTime = 1f;
+
+    private float bonusTime = 2f;
+    private float disappearTime = 5f;
+
+    private int scoreValue = 10; //通常のスコア値
+    private int bonusScoreValue = 20; //ボーナススコア値
+
+    private int bigBonusScoreValue = 100; //
+
+    private enum bonusChance
+    {
+        Invalide = -1,
+        BigBonus,
+        SmallBonus,
+        NoBonus
+    }
+
+    private bonusChance bonusChances;
+
+    public Material CoinMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(BonusTimeCoroutine());
+    }
+
+    IEnumerator BonusTimeCoroutine()
+    {
+
+        bonusChances = bonusChance.BigBonus;
+        CoinMaterial.color = Color.yellow;
+        yield return new WaitForSeconds(bigBonusTime);
+        bonusChances = bonusChance.SmallBonus;
+        CoinMaterial.color = Color.cyan;
+        yield return new WaitForSeconds(bonusTime - bigBonusTime);
+        bonusChances = bonusChance.NoBonus;
+        CoinMaterial.color = Color.red;
+        yield return new WaitForSeconds(disappearTime - bonusTime);
+        Destroy(this.gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Sphere")
+        {
+            switch (bonusChances)
+            {
+                case bonusChance.BigBonus:
+                    ScoreManager.ScoreCount += bonusScoreValue;
+                    break;
+                case bonusChance.SmallBonus:
+                    ScoreManager.ScoreCount += bonusScoreValue;
+                    break;
+                case bonusChance.NoBonus:
+                    ScoreManager.ScoreCount += scoreValue;
+                    break;
+            }
+
+            //コインを消去
+            Destroy(gameObject);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
